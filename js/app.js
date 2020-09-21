177095adce793b40d8afa7cd63f10b60a458e07d
Amit <amit.kumar.tiwary@sap.com>
2020-09-21 14:47:48 +0530

    Adding comments and indentation

--- js/app.js
+++ js/app.js
@@ -1,43 +1,46 @@
-const apiKey = 'Request new API Key from newsAPI.org'; 
-const defaultSource = ''; // default source of news gb is UK
+const apiKey = 'Replace with new API Key from newsAPI.org or use one shared';
+const defaultSource = ''; //default setting to search for all.
 const sourceSelector = document.querySelector('#news-keyword');
 const newsArticles = document.querySelector('#news-list');
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () =>
-    navigator.serviceWorker.register('./serviceWorker.js')
-    .then(registration => console.log('Service Worker registered'))
-    .catch(err => 'Service worker registration failed'));
+    window.addEventListener('load', () =>
+        navigator.serviceWorker.register('./serviceWorker.js')
+        .then(registration => console.log('Service Worker registered'))
+        .catch(err => 'Service worker registration failed'));
 }
 
 window.addEventListener('load', e => {
-  sourceSelector.addEventListener('change', evt => updateNews(evt.target.value));
-  updateNewsSources().then(() => {
-    sourceSelector.value = defaultSource;
-    updateNews();
-  });
+    sourceSelector.addEventListener('change', evt => updateNews(evt.target.value));
+    updateNewsSources().then(() => {
+        sourceSelector.value = defaultSource;
+        updateNews();
+    });
 });
-
+// Handling for network online/offline scenerio
 window.addEventListener('online', () => updateNews(sourceSelector.value));
 
+//Setting default list to "gb" for UK based news as asked in problem statements.
 async function updateNewsSources() {
-  const response = await fetch(`https://newsapi.org/v2/top-headlines?country=gb&apiKey=${apiKey}`);
-  const json = await response.json();
-  sourceSelector.innerHTML =
-    json.sources;
+    const response = await fetch(`https://newsapi.org/v2/top-headlines?country=gb&apiKey=${apiKey}`);
+    const json = await response.json();
+    sourceSelector.innerHTML =
+        json.sources;
 }
 
+//When search terms/keywords are changed
 async function updateNews(source = defaultSource) {
-  newsArticles.innerHTML = '';
-  const response = await fetch(`https://newsapi.org/v2/top-headlines?q=${source}&sortBy=top&cgit &apiKey=${apiKey}`);
-  const json = await response.json();
-  console.log('Articles fetched', json);
-  newsArticles.innerHTML =
-    json.articles.map(createArticle).join('\n');
+    newsArticles.innerHTML = '';
+    const response = await fetch(`https://newsapi.org/v2/top-headlines?q=${source}&sortBy=top&cgit &apiKey=${apiKey}`);
+    const json = await response.json();
+    console.log('Articles fetched', json);
+    newsArticles.innerHTML =
+        json.articles.map(createArticle).join('\n');
 }
 
+//Designing the appearance of card and associate with the data being received and setting click action for complete card to open full news.
 function createArticle(article) {
-  return `
+    return `
       <a class="story" href="${article.url}">
         <img class="story-image" src="${article.urlToImage}" alt="${article.title}">
         <p class="headline">${article.title}</p>
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,44 +1,45 @@
 const cacheName = 'news-v1';
 const staticAssets = [
-  './',
-  './js/app.js',
-  './css/style.css',
-  './fallback.json',
-  './images/fetch-dog.jpg'
+    './',
+    './js/app.js',
+    './css/style.css',
+    './fallback.json',
+    './images/fetch-dog.jpg'
 ];
 
-self.addEventListener('install', async function () {
-  const cache = await caches.open(cacheName);
-  cache.addAll(staticAssets);
+self.addEventListener('install', async function() {
+    const cache = await caches.open(cacheName);
+    cache.addAll(staticAssets);
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
-  const request = event.request;
-  const url = new URL(request.url);
-  if (url.origin === location.origin) {
-    event.respondWith(cacheFirst(request));
-  } else {
-    event.respondWith(networkFirst(request));
-  }
+    const request = event.request;
+    const url = new URL(request.url);
+    if (url.origin === location.origin) {
+        event.respondWith(cacheFirst(request));
+    } else {
+        event.respondWith(networkFirst(request));
+    }
 });
-
+//caching the first request
 async function cacheFirst(request) {
-  const cachedResponse = await caches.match(request);
-  return cachedResponse || fetch(request);
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || fetch(request);
 }
-
+//setting dynamic caching for better performance
 async function networkFirst(request) {
-  const dynamicCache = await caches.open('news-dynamic');
-  try {
-    const networkResponse = await fetch(request);
-    dynamicCache.put(request, networkResponse.clone());
-    return networkResponse;
-  } catch (err) {
-    const cachedResponse = await dynamicCache.match(request);
-    return cachedResponse || await caches.match('../fallback.json');
-  }
+    const dynamicCache = await caches.open('news-dynamic');
+    try {
+        const networkResponse = await fetch(request);
+        dynamicCache.put(request, networkResponse.clone());
+        return networkResponse;
+    } catch (err) {
+        const cachedResponse = await dynamicCache.match(request);
+        //adding fallbacks in case of api fetching fails due to no network
+        return cachedResponse || await caches.match('../fallback.json');
+    }
 }
